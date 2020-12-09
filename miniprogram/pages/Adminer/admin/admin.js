959f25f75f10efe48cba8eb1c401a7f7985a52e2
何绍玻 <>
2020-12-09 11:39:12 +0800

    v1.0.8.1
    管理员退出登录

--- miniprogram/pages/Adminer/admin/admin.js
+++ miniprogram/pages/Adminer/admin/admin.js
@@ -5,57 +5,62 @@ Page({
    * 页面的初始数据
    */
 
-  data:{
-    userListInfo:[{
-      icon:"/images/重置.png",
-      text:'重置用户密码'
-    },{
-      icon:"/images/查询.png",
-      text:'查询跑步数据'
-    },{
-      icon:"/images/注销.png",
-      text:'注销用户账号'
-    },{
-      icon:"/images/修改.png",
-      text:'修改学生信息'
-    }
-    ]
-      },
-    onLoad: function(){
-      var that = this
-      wx.getUserInfo(function (userInfo){
-        that.setData({
-          userInfo:userInfo
-        })
+  data: {
+    userListInfo: [{
+      icon: "/images/重置.png",
+      text: '重置用户密码'
+    }, {
+      icon: "/images/查询.png",
+      text: '查询跑步数据'
+    }, {
+      icon: "/images/注销.png",
+      text: '注销用户账号'
+    }, {
+      icon: "/images/修改.png",
+      text: '修改学生信息'
+    }]
+  },
+  exit: function () {
+    wx.redirectTo({
+      url: '../../login/login',
+      
+    })
+  },
+  onLoad: function () {
+    var that = this
+    wx.getUserInfo(function (userInfo) {
+      that.setData({
+        userInfo: userInfo
       })
-    },
-    move:function(event){
-      var path =event.currentTarget.id
-      console.log(path)
-      switch(path) {
-        case '0':
-          console.log(path)
-           wx.navigateTo({
-             url: '../adminReset/adminReset',
-           });
-           break;
-        case '1':
-          wx.navigateTo({
-            url: '../adminSel/adminSel',
-          });
-           break;
-        case '2':
-          wx.navigateTo({
-            url: '../adminCanel/adminCanel',
-          });
-            break;
-        case '3':
-          wx.navigateTo({
-            url: '../adminUpdate/adminUpdate',
-          });
-            break;
-   } 
-      },
+    })
+  },
+  move: function (event) {
+    var path = event.currentTarget.id
+    console.log(path)
+    switch (path) {
+      case '0':
+        console.log(path)
+        wx.navigateTo({
+          url: '../adminReset/adminReset',
+        });
+        break;
+      case '1':
+        wx.navigateTo({
+          url: '../adminSel/adminSel',
+        });
+        break;
+      case '2':
+        wx.navigateTo({
+          url: '../adminCanel/adminCanel',
+        });
+        break;
+      case '3':
+        wx.navigateTo({
+          url: '../adminUpdate/adminUpdate',
+        });
+        break;
+    }
+  },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
